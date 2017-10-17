64e87cb5cc9371a075cbea6a9c23e448d64eb3b5
Pooh3Mobi <pooh3.mobi@gmail.com>
2017-10-17 23:25:19 +0900

    simplify

--- app/src/main/kotlin/mobi/pooh3/frpstudy/extensions/RxSodium.kt
+++ app/src/main/kotlin/mobi/pooh3/frpstudy/extensions/RxSodium.kt
@@ -1,13 +1,11 @@
 package mobi.pooh3.frpstudy.extensions
 
 import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.BehaviorSubject
 
-fun <T> Observable<T>.hold(t: T): BehaviorSubject<T> =
-        let { o ->
-            BehaviorSubject.createDefault(t)
-                    .apply { o.subscribe(this) }
-        }
+fun <T> Observable<T>.hold(value: T): BehaviorSubject<T> =
+        BehaviorSubject.createDefault(value).apply { this@hold.subscribe(this) }
 
-fun <T> BehaviorSubject<T>.loop(s: Observable<T>): Unit =
-        let{ c -> s.subscribe { value -> c.onNext(value) } }
+fun <T> BehaviorSubject<T>.loop(stream: Observable<T>): Disposable =
+        stream.subscribe { value -> this.onNext(value) }
