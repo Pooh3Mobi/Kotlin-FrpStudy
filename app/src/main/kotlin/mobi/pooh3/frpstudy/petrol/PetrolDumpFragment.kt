b18d558f67895fceec510c51ce402617d0013a12
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-03-04 12:48:14 +0900

    add fragment

---
+++ app/src/main/kotlin/mobi/pooh3/frpstudy/petrol/PetrolDumpFragment.kt
@@ -0,0 +1,116 @@
+package mobi.pooh3.frpstudy.petrol
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+import android.media.ToneGenerator
+import android.media.AudioManager
+import io.reactivex.rxkotlin.withLatestFrom
+import mobi.pooh3.frpstudy.R
+import mobi.pooh3.frpstudy.extensions.hold
+import mobi.pooh3.frpstudy.extensions.loop
+import mobi.pooh3.frpstudy.petrol.domain.End
+import mobi.pooh3.frpstudy.petrol.domain.Fuel
+import mobi.pooh3.frpstudy.petrol.domain.UpDown
+import mobi.pooh3.frpstudy.rx.unOptional
+import java.util.*
+
+
+class LiveCycle(sNozzle1: Observable<UpDown>, sNozzle2: Observable<UpDown>, sNozzle3: Observable<UpDown>) {
+    val sStart: Observable<Fuel>
+    val sEnd: Observable<End>
+    val fillActive: BehaviorSubject<Optional<Fuel>>
+
+    init {
+        val sLiftNozzle =
+                Observable.merge(
+                        whenLifted(sNozzle1, Fuel.ONE),
+                        whenLifted(sNozzle2, Fuel.TWO),
+                        whenLifted(sNozzle3, Fuel.THREE)
+                )
+
+        this.fillActive = BehaviorSubject.create()
+
+        this.sStart =
+                sLiftNozzle
+                        .withLatestFrom(fillActive, { newFuel, fillActive_ ->
+                                    if (fillActive_ == null)
+                                        Optional.of(newFuel)
+                                    else
+                                        Optional.empty()
+                                }
+                        )
+                        .unOptional()
+
+        this.sEnd =
+                Observable.merge(
+                        whenSetDown(sNozzle1, Fuel.ONE, fillActive),
+                        whenSetDown(sNozzle2, Fuel.TWO, fillActive),
+                        whenSetDown(sNozzle3, Fuel.THREE, fillActive)
+                )
+
+        this.fillActive.loop(
+                Observable
+                        .merge(
+                                sEnd.map   { Optional.empty<Fuel>() },
+                                sStart.map { Optional.of(it) }
+                        )
+                        .hold(Optional.empty())
+        )
+    }
+}
+
+fun whenLifted(sNozzle: Observable<UpDown>, nozzleFuel: Fuel): Observable<Fuel> =
+        sNozzle
+                .filter { u -> u == UpDown.UP }
+                .map { nozzleFuel }
+
+fun whenSetDown(sNozzle: Observable<UpDown>, nozzleFuel: Fuel, fillActive: BehaviorSubject<Optional<Fuel>>): Observable<End> =
+        sNozzle
+                .withLatestFrom(fillActive, { u: UpDown, f: Optional<Fuel> ->
+                            if (u == UpDown.DOWN && f == Optional.of(nozzleFuel))
+                                Optional.of(End.END)
+                            else
+                                Optional.empty()
+                        }
+                )
+                .unOptional()
+
+fun beep() {
+    val toneGenerator = ToneGenerator(AudioManager.STREAM_SYSTEM, ToneGenerator.MAX_VOLUME/2)
+    toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP)
+}
+
+
+
+
+
+
+
+
+
+class PetrolDumpFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_petrol_dump, container, false)
+    }
+
+    override fun onViewCreated(v: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(v!!, savedInstanceState)
+    }
+
+
+    companion object {
+        fun newInstance(): PetrolDumpFragment {
+            return PetrolDumpFragment()
+        }
+    }
+
+}
+
+
